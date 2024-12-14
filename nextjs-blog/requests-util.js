175ba3b6adaebb06df6e8de3ae464df5e18b33ca
Meivuu <70550220+May0san@users.noreply.github.com>
2024-12-14 15:35:41 -0700

    yo

---
+++ nextjs-blog/requests-util.js
@@ -0,0 +1,13 @@
+
+
+function messageIDRequest(request){
+    
+}
+
+function quickRequest(request){
+    processRequest(request.nextUrl.pathname.startsWith('/Q&L'))
+}
+
+function processRequest(string){
+
+}
